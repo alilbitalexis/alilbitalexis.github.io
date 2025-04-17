0d1046198ed5e372717718e1d4b21d208aac8c53
Alexis Elliott <alexise72523@gmail.com>
2025-04-17 14:02:21 -0400

    Added mobile menu.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,18 @@
 // Data
+import { useState } from "react";
 import { links } from "../data/config";
 import Nav from "./nav";
 
 export default function Header() {
+  const [active, setActive] = useState(false);
+
   function scrollTo(e, id) {
     e.preventDefault();
     document.querySelector(`#${id}`).scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+
+  function menuClick() {
+    setActive(!active);
   }
 
   // For mobile: two areas/columns: social and menu
@@ -14,7 +21,12 @@ export default function Header() {
       <div className="logo" onClick={(e) => scrollTo(e, "root")}>
         <img src="./images/avatar.png" alt="" /> Alexis Elliott
       </div>
-      <Nav />
+      <Nav mobile={active} setMobile={setActive} />
+      <div className={`mobile-menu-icon${active ? " active" : ""}`} onClick={menuClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </header>
   );
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,20 +1,26 @@
 // Data
+import { useState } from "react";
 import { links } from "../data/config";
 
-export default function Nav() {
+export default function Nav({ mobile, setMobile }) {
   function scrollTo(e, id) {
     e.preventDefault();
     document.querySelector(`#${id}`).scrollIntoView({ behavior: "smooth", block: "start" });
+    if (mobile) {
+      setMobile(false);
+    }
   }
 
   // For mobile: two areas/columns: social and menu
   return (
-    <nav>
+    <nav className={mobile ? "mobile" : ""}>
+      {mobile && <h4>Menu</h4>}
       {links.map((l, key) => (
         <a key={key} onClick={(e) => scrollTo(e, l.id)} href="">
           {l.label}
         </a>
       ))}
+      {mobile && <h4>Social</h4>}
     </nav>
   );
 }
