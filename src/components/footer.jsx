5e3c621b83e57b6d22542ea51332fd188eaae00e
Alexis Elliott <alexise72523@gmail.com>
2025-04-18 11:11:23 -0400

    Fixed footer layout and header layer issue.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,15 +6,16 @@ export default function Footer() {
   const date = new Date();
   return (
     <footer>
-      {/* <div className="footer-row"> */}
-      <div className="copyright">
-        ©{date.getFullYear()}{" "}
-        <a href="https://github.com/alilbitalexis" target="_blank">
-          Alexis Elliott
-        </a>
-        . Code repository on <a href="https://github.com/alilbitalexis/alilbitalexis.github.io">GitHub</a>.
+      <div className="container">
+        <div className="copyright">
+          ©{date.getFullYear()}{" "}
+          <a href="https://github.com/alilbitalexis" target="_blank">
+            Alexis Elliott
+          </a>
+          . Code repository on <a href="https://github.com/alilbitalexis/alilbitalexis.github.io">GitHub</a>.
+        </div>
+        <Nav />
       </div>
-      <Nav />
     </footer>
   );
 }
